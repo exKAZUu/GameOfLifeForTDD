1ccd06b03654df12481b3332358b89211352e11b
Kohsuke Yatoh <kohyatoh@gmail.com>
2015-02-16 19:20:18 +0900

    test

--- src/test/java/jp/ac/nii/GameOfLifeTest.java
+++ src/test/java/jp/ac/nii/GameOfLifeTest.java
@@ -13,4 +13,19 @@ public class GameOfLifeTest {
     assertNotEquals(null, new GameOfLife(10, 10));
     assertNotNull(new GameOfLife(10, 10));
   }
+
+  @Test
+  public void testDeadToAlive() {
+    Field field = new Field(2, 2);
+    field.clear();
+    field.setNextCell(0, 1, true);
+    field.setNextCell(1, 1, true);
+    field.setNextCell(1, 0, true);
+    field.update();
+    GameOfLife game = new GameOfLife(field);
+    game.start();
+    game.advance();
+    assertTrue(field.isLiving(0, 0));
+  }
+  
 }
