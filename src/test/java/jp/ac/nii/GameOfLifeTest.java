35051bc34b7707bb7798702fe1919a8b2e4d76cc
Yuta Tokitake <toki211@gmail.com>
2015-02-16 16:16:43 +0900

    test

--- src/test/java/jp/ac/nii/GameOfLifeTest.java
+++ src/test/java/jp/ac/nii/GameOfLifeTest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameOfLifeTest {
 
   @Test
@@ -12,5 +15,23 @@ public class GameOfLifeTest {
     assertThat(new GameOfLife(10, 10), is(not(nullValue())));
     assertNotEquals(null, new GameOfLife(10, 10));
     assertNotNull(new GameOfLife(10, 10));
+  }
+
+  @Test
+  public void testBirth() {
+    GameOfLife game = new GameOfLife(3, 3);
+    Field f = game.getField();
+    f.clear();
+    f.initializeCell(0, 0, true);
+    f.initializeCell(1, 0, true);
+    f.initializeCell(0, 1, true);
+    f.update();
+
+    boolean[] res = new boolean[9];
+    for (int i = 0; i < 9; i++) {
+      res[i] = f.isLiving(i/3, i%3);
+    }
+    assertArrayEquals(res,
+                      new boolean[]{true, true, false, true, true, false, false, false, false});
   }
 }
