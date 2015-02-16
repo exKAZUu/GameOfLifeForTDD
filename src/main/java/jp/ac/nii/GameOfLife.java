fa0787fff123946f12842182ad0022ee22627229
Yuta Tokitake <toki211@gmail.com>
2015-02-16 16:36:17 +0900

    impl

--- src/main/java/jp/ac/nii/GameOfLife.java
+++ src/main/java/jp/ac/nii/GameOfLife.java
@@ -47,9 +47,38 @@ public class GameOfLife {
     }
     
     // ここに処理を追記して完成させよう
-    
+    int w = _field.getWidth();
+    int h = _field.getHeight();
+    for (int y = 0; y < h; y++) {
+      for (int x = 0; x < w; x++) {
+        boolean isLiving = _field.isLiving(x, y);
+        int c = countLivingNeighborCells(x, y);
+        if (!isLiving && c == 3)
+          birth(x, y);
+      }
+    }
+
     _field.update();
   }
   
   // メソッドを適宜追加しよう
+  public void birth(int x, int y) {
+    _field.setNextCell(x, y, true);
+  }
+
+  public int countLivingNeighborCells(int x, int y) {
+    int c = 0;
+    for (int _y = y - 1; _y <= y + 1; _y++) {
+      if (_y < 0 || _y >= _field.getHeight())
+        continue;
+      for (int _x = x - 1; _x <= x + 1; _x++) {
+        if (_x < 0 || _x >= _field.getWidth())
+          continue;
+
+        if (_field.isLiving(_x, _y))
+          c++;
+      }
+    }
+    return c;
+  }
 }
