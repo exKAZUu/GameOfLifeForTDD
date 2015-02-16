f0a8066908e109c277b7e6e4144b523d6a50fe31
Junichi Kobayashi <nnujabok@gmail.com>
2015-02-17 00:44:33 +0900

    test

--- src/test/java/jp/ac/nii/GameOfLifeTest.java
+++ src/test/java/jp/ac/nii/GameOfLifeTest.java
@@ -13,4 +13,24 @@ public class GameOfLifeTest {
     assertNotEquals(null, new GameOfLife(10, 10));
     assertNotNull(new GameOfLife(10, 10));
   }
+  
+  @Test
+  public void testAdvance() {
+    Field field = new Field(3, 3);
+    for(int i = 0; i < 5; i++)
+      for(int j = 0; j < 5; j++)
+        field.initializeCell(i, j, (i + j) % 2 == 0);
+    GameOfLife gameOfLife = new GameOfLife(field);
+    gameOfLife.advance();
+    field = gameOfLife.getField();
+    assertTrue(field.isLiving(1, 0));
+    assertTrue(field.isLiving(0, 1));
+    assertTrue(field.isLiving(1, 2));
+    assertTrue(field.isLiving(2, 1));
+    assertFalse(field.isLiving(0, 0));
+    assertFalse(field.isLiving(0, 2));
+    assertFalse(field.isLiving(1, 1));
+    assertFalse(field.isLiving(2, 0));
+    assertFalse(field.isLiving(2, 2));
+  }
 }
